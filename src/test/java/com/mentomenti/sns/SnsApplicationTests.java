ddb9dd6af0fcae4c761e5e00b567469405d74b81
RungDaRongDa <jbj2720@gmail.com>
2023-05-31 12:59:44 +0900

    초기 설정

---
+++ src/test/java/com/mentomenti/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentomenti.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
